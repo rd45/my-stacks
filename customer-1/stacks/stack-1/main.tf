de0d4205a62c13f1731654f567c7152611c3bfbb
Richard Dewes <56639028+rd45@users.noreply.github.com>
2019-10-25 11:19:11 +0100

    very simple vcn-only stack

---
+++ customer-1/stacks/stack-1/main.tf
@@ -0,0 +1,27 @@
+// Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+
+provider "oci" {
+  tenancy_ocid     = "${var.tenancy_ocid}"
+  user_ocid        = "${var.user_ocid}"
+  fingerprint      = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region           = "${var.region}"
+}
+
+resource "oci_core_vcn" "vcn1" {
+  cidr_block     = "10.0.0.0/16"
+  dns_label      = "vcn1"
+  compartment_id = "${var.compartment_ocid}"
+  display_name   = "vcn1"
+}
+
+output "vcn_id" {
+  value = "${oci_core_vcn.vcn1.id}"
+}
